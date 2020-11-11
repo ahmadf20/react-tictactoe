5d4b609ff835c1d036e3bdbbdf10cd1ec6e08ba0
Ahmad Faaiz Al-auza'i <afaaiz2@gmail.com>
2020-11-11 21:56:40 +0700

    Updates

---
+++ precache-manifest.697d9f37de263063fdc85fcf73c24c63.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "646f199ac94738df5d5b59733f2fbe71",
+    "url": "/react-tictactoe/index.html"
+  },
+  {
+    "revision": "4d201ad99681b9ec66c8",
+    "url": "/react-tictactoe/static/css/2.dec86d71.chunk.css"
+  },
+  {
+    "revision": "3bbae6d83300ee621fe1",
+    "url": "/react-tictactoe/static/css/main.6635388b.chunk.css"
+  },
+  {
+    "revision": "4d201ad99681b9ec66c8",
+    "url": "/react-tictactoe/static/js/2.cf1ca9c8.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-tictactoe/static/js/2.cf1ca9c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bbae6d83300ee621fe1",
+    "url": "/react-tictactoe/static/js/main.6e8c5157.chunk.js"
+  },
+  {
+    "revision": "3fa13acf8cc8b96bed84",
+    "url": "/react-tictactoe/static/js/runtime-main.4821dfcf.js"
+  }
+]);
